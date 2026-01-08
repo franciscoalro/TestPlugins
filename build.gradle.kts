909ccc1d9115e0898b7ab296ca185e09df344504
CloudstreamDev <dev@cloudstream.local>
2026-01-07 21:11:58 -0300

    chore: upgrade Kotlin to 2.2.21 to match dependency metadata version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1") // Versão mais estável
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT") // Usar master-SNAPSHOT explícito
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21") // Kotlin 2.0 para compatibilidade
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21") // Kotlin 2.2 para compatibilidade com dependências
     }
 }
 

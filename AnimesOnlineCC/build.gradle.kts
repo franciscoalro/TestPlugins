9a20029de4728217f01689951b05dd6368dd81f1
CloudstreamDev <dev@cloudstream.local>
2026-01-07 21:36:08 -0300

    chore: bump versions - AnimesOnlineCC v6, MaxSeries v3

--- AnimesOnlineCC/build.gradle.kts
+++ AnimesOnlineCC/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 5
+version = 6
 
 cloudstream {
     description = "Assista animes online grátis em HD"
--- MaxSeries/build.gradle.kts
+++ MaxSeries/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 2
+version = 3
 
 cloudstream {
     description = "Assista séries online em HD"

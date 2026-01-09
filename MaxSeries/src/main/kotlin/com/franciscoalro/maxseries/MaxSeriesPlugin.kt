380860842c3cf5cdc7868b3c8c048d9e14516bfc
CloudstreamDev <dev@cloudstream.local>
2026-01-08 22:03:37 -0300

    fix: MaxSeries v20.0 - Kotlin 2.2.21 exato + Plugin class corrigida

--- MaxSeries/src/main/kotlin/com/franciscoalro/maxseries/MaxSeriesPlugin.kt
+++ MaxSeries/src/main/kotlin/com/franciscoalro/maxseries/MaxSeriesPlugin.kt
@@ -1,10 +1,10 @@
 package com.franciscoalro.maxseries
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class MaxSeriesPlugin: BasePlugin() {
+class MaxSeriesPlugin: Plugin() {
     override fun load() {
         // Registra o provedor de séries
         registerMainAPI(MaxSeriesProvider())

8d2a46e24e47f1dddebb67825e827158710753b4
franciscoalro <franciscoalro@users.noreply.github.com>
2026-01-09 01:19:38 +0000

    deploy: 6f869b39540616298abe8192c4881bf6e2d65d64

--- MaxSeries/build.gradle.kts
+++ MaxSeries/build.gradle.kts
@@ -1,7 +1,7 @@
-version = 21
+version = 22
 
 cloudstream {
-    description = "MaxSeries v21.0 - Plugin load(context) + newExtractorLink posicional"
+    description = "MaxSeries v22.0 - BasePlugin + newExtractorLink com initializer"
     authors = listOf("franciscoalro")
     status = 1
     tvTypes = listOf("TvSeries", "Movie")

4fceb78375b9118a3e91c80a740f857596c2d035
CloudstreamDev <dev@cloudstream.local>
2026-01-10 17:42:04 -0300

    Fix build error: Downgrade Kotlin to 1.9.23 (stable)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 

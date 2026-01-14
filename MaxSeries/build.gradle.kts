15bd1fc5fdc6d3711f0bfd7435e35ccb6bca02eb
franciscoalro <franciscoalro@users.noreply.github.com>
2026-01-14 00:06:15 +0000

    deploy: 8bb7555c4319131dd7c48eb1e899760705ec7500

--- MaxSeries/build.gradle.kts
+++ MaxSeries/build.gradle.kts
@@ -1,7 +1,7 @@
-version = 77
+version = 78
 
 cloudstream {
-    description = "MaxSeries v77 - 10 extractors suportados (Jan 2026): PlayerEmbedAPI, MyVidPlay, StreamTape, Dood, Mixdrop, FileMoon, Uqload, VidCloud, UpStream, MegaEmbed"
+    description = "MaxSeries v78 - Busca corrigida + 10 extractors (Jan 2026): PlayerEmbedAPI, MyVidPlay, StreamTape, Dood, Mixdrop, FileMoon, Uqload, VidCloud, UpStream, MegaEmbed"
     authors = listOf("franciscoalro")
     status = 1
     tvTypes = listOf("TvSeries", "Movie")

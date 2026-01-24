0a5dfec3cbb2088c578ae8025e305238fbfd7799
franciscoalro <franciscoalro@users.noreply.github.com>
2026-01-24 17:52:42 +0000

    deploy: 1f1b71891818787df9fedb3c0918bff4b5f7d4ac

--- MaxSeries/src/main/kotlin/com/franciscoalro/maxseries/MaxSeriesProvider.kt
+++ MaxSeries/src/main/kotlin/com/franciscoalro/maxseries/MaxSeriesProvider.kt
@@ -57,7 +57,7 @@ class MaxSeriesProvider : MainAPI() {
     }
     
     init {
-        Log.wtf(TAG, "🚀🚀🚀 MAXSERIES PROVIDER v177 CARREGADO! 🚀🚀🚀")
+        Log.wtf(TAG, "🚀🚀🚀 MAXSERIES PROVIDER v178 CARREGADO! 🚀🚀🚀")
         Log.wtf(TAG, "Name: $name, MainUrl: $mainUrl")
     }
 
@@ -698,7 +698,7 @@ class MaxSeriesProvider : MainAPI() {
             }
         }
         
-        Log.d(TAG, "📋 Total sources extraídas (v177): ${sources.size} - $sources")
+        Log.d(TAG, "📋 Total sources extraídas (v178): ${sources.size} - $sources")
         return sources.distinct()
     }
 }

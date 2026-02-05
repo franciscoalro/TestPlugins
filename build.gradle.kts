336b9039ca96f1b83c1908dd4d4f472d613d9848
franciscoalro <franciscoalro@users.noreply.github.com>
2026-02-05 15:27:59 +0000

    deploy: a987609285488002d6fca8f3c9bccd65d7327dcb

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
-        classpath("com.github.recloudstream:gradle:cce1b8d84d") {
-            exclude(group = "com.github.vidstige", module = "jadb")
-        }
-        classpath("com.github.vidstige:jadb:v1.2.1")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
@@ -25,16 +22,6 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-    }
-    
-    configurations.all {
-        resolutionStrategy {
-            force("org.jetbrains.kotlin:kotlin-stdlib:2.3.0")
-            force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.3.0")
-            force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.3.0")
-            force("org.jetbrains.kotlin:kotlin-stdlib-common:2.3.0")
-            force("org.jetbrains.kotlin:kotlin-reflect:2.3.0")
-        }
     }
 }
 
@@ -48,7 +35,8 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "franciscoalro/TestPlugins")
+        setRepo("https://github.com/franciscoalro/TestPlugins")
+        authors = listOf("franciscoalro")
     }
 
     android {
@@ -78,24 +66,15 @@ subprojects {
 
     dependencies {
         val implementation by configurations
-
-        implementation("com.github.recloudstream.cloudstream:library:8a4480dc42") // Commit hash estável (fix JitPack)
+        implementation("com.github.recloudstream.cloudstream:library:master-SNAPSHOT")
         implementation(kotlin("stdlib", "2.3.0"))
         implementation("com.github.Blatzar:NiceHttp:0.4.13")
         implementation("org.jsoup:jsoup:1.19.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
         implementation("com.squareup.okhttp3:okhttp:4.12.0")
         implementation("androidx.webkit:webkit:1.8.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
-        
-        // Ferramentas do saimuelrepo-main
-        implementation("org.mozilla:rhino:1.8.0")
-        implementation("app.cash.quickjs:quickjs-android:0.9.2")
-        implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.11.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-        implementation("com.github.vidstige:jadb:v1.2.1")
     }
 }
 
